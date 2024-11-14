0f7dcff50b09f6bb01fcd3d5841917bbb1aa60b6
balendu9 <balendurawat9@gmail.com>
2024-11-14 17:17:27 +0530

    update

--- Components/Partners.jsx
+++ Components/Partners.jsx
@@ -5,42 +5,42 @@ const Partners = () => {
     {
       name: "Techs Network",
       image: "img/partners/logo1.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo2.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo3.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo4.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo5.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo6.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo7.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
     {
       name: "Techs Network",
       image: "img/partners/logo8.svg",
-      url: "https://www.Techs Network/pro-nft-marketplace",
+      url: "https://www.TechsNetwork/pro-nft-marketplace",
     },
   ];
   return (
@@ -49,7 +49,7 @@ const Partners = () => {
         <div className="row">
           <div className="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-8 offset-xl-2">
             <div className="section__title">
-              <h2>Our Partners</h2>
+              <h2>Communities</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
           </div>
@@ -57,7 +57,7 @@ const Partners = () => {
         <div className="row">
           {partners.map((partner, index) => (
             <div key={index} className="col-6 col-lg-3">
-              <a href={partner.link} className="partner"><p>{partner.name}</p></a>
+              <a href={partner.link} className="partner"><p><img src={partner.image} alt="" />{partner.name}</p></a>
             </div>
           ))}
         </div>
